5192ccb46f7cc9640a3070eba1c9d299424c34ed
Justin Carstens <justin.carstens01@gmail.com>
2024-02-10 12:35:08 +0200

    Added Store Page redirect tests for Twitter, Facebook & LinkedIn

--- OnlineShop/src/test/java/tests/StorePageTest.java
+++ OnlineShop/src/test/java/tests/StorePageTest.java
@@ -1,9 +1,18 @@
 package tests;
 
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import utils.WebDriverManager;
+
+import java.time.Duration;
+import java.util.List;
 
 public class StorePageTest {
 
@@ -12,6 +21,28 @@ public class StorePageTest {
     @BeforeAll
     public static void setUp() {
         driver = WebDriverManager.getDriver();
+        driver.get("https://qa-challenge.codesubmit.io/");
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+    }
+
+    @Test
+    public void testTwitterRedirect() {
+        String link = driver.findElement(By.className("social_twitter")).findElement(By.tagName("a")).getAttribute("href");
+        Assertions.assertEquals("https://twitter.com/saucelabs", link);
+    }
+
+    @Test
+    public void testFacebookRedirect() {
+        String link = driver.findElement(By.className("social_facebook")).findElement(By.tagName("a")).getAttribute("href");
+        Assertions.assertEquals("https://www.facebook.com/saucelabs", link);
+    }
+
+    @Test
+    public void testLinkedinRedirect() {
+        String link = driver.findElement(By.className("social_linkedin")).findElement(By.tagName("a")).getAttribute("href");
+        Assertions.assertEquals("https://www.linkedin.com/company/sauce-labs/", link);
     }
 
     @AfterAll
