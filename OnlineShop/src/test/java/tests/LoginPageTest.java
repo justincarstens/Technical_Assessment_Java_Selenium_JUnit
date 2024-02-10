04132c072db9b436c31e987af077de979b1e94e7
Justin Carstens <justin.carstens01@gmail.com>
2024-02-10 12:12:24 +0200

    Added testStandardUserLogOutFromStore

--- OnlineShop/src/test/java/tests/LoginPageTest.java
+++ OnlineShop/src/test/java/tests/LoginPageTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import utils.WebDriverManager;
@@ -44,6 +43,22 @@ public class LoginPageTest {
         WebElement errorMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("h3[data-test='error']")));
 
         Assertions.assertEquals("Epic sadface: Username and password do not match any user in this service", errorMessage.getText());
+    }
+
+    @Test
+    public void testStandardUserLogOutFromStore() {
+        driver.get("https://qa-challenge.codesubmit.io/");
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+        driver.findElement(By.id("react-burger-menu-btn")).click();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement element = wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.id("logout_sidebar_link"))));
+        element.click();
+
+        Assertions.assertEquals("https://qa-challenge.codesubmit.io/", driver.getCurrentUrl());
+
     }
 
     @AfterAll
