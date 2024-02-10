dba3b39cd2908c0f2b438a39540a0d7fc53f4639
Justin Carstens <justin.carstens01@gmail.com>
2024-02-10 11:38:27 +0200

    Added WebDriver setUp and tearDown boiler plate code to each class

--- OnlineShop/src/test/java/tests/LoginPageTest.java
+++ OnlineShop/src/test/java/tests/LoginPageTest.java
@@ -1,4 +1,21 @@
 package tests;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.WebDriver;
+import utils.WebDriverManager;
+
 public class LoginPageTest {
+
+    public static WebDriver driver;
+
+    @BeforeAll
+    public static void setUp() {
+        driver = WebDriverManager.getDriver();
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        WebDriverManager.closeDriver();
+    }
 }
--- OnlineShop/src/test/java/tests/StorePageTest.java
+++ OnlineShop/src/test/java/tests/StorePageTest.java
@@ -1,4 +1,22 @@
 package tests;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.WebDriver;
+import utils.WebDriverManager;
+
 public class StorePageTest {
+
+    public static WebDriver driver;
+
+    @BeforeAll
+    public static void setUp() {
+        driver = WebDriverManager.getDriver();
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        WebDriverManager.closeDriver();
+    }
+
 }
