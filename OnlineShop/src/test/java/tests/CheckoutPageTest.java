04b2abcb9b504eefec253b81131fc410ca349089
Justin Carstens <justin.carstens01@gmail.com>
2024-02-10 14:13:48 +0200

    Added testCheckOutProcess

--- OnlineShop/src/test/java/tests/CheckoutPageTest.java
+++ OnlineShop/src/test/java/tests/CheckoutPageTest.java
@@ -1,8 +1,12 @@
 package tests;
 
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import utils.WebDriverManager;
 
 public class CheckoutPageTest {
@@ -12,6 +16,25 @@ public class CheckoutPageTest {
     @BeforeAll
     public static void setUp() {
         driver = WebDriverManager.getDriver();
+        driver.get("https://qa-challenge.codesubmit.io/");
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+    }
+
+    @Test
+    public void testCheckOutProcess() {
+        driver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
+        driver.findElement(By.cssSelector(".shopping_cart_link")).click();
+        driver.findElement(By.id("checkout")).click();
+        driver.findElement(By.id("first-name")).sendKeys("Justin");
+        driver.findElement(By.id("last-name")).sendKeys("Carstens");
+        driver.findElement(By.id("postal-code")).sendKeys("7550");
+        driver.findElement(By.id("continue")).click();
+        driver.findElement(By.id("finish")).click();
+
+        WebElement element = driver.findElement(By.className("complete-header"));
+        Assertions.assertEquals("Thank you for your order!", element.getText());
     }
 
     @AfterAll
