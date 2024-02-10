893c020e83c64c7c44b856776330bfe7d4f8004e
Justin Carstens <justin.carstens01@gmail.com>
2024-02-10 12:04:35 +0200

    Added testStandardUserLogin & testStandardUserFailedLogin

--- OnlineShop/src/test/java/tests/LoginPageTest.java
+++ OnlineShop/src/test/java/tests/LoginPageTest.java
@@ -1,9 +1,18 @@
 package tests;
 
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import utils.WebDriverManager;
+
+import java.time.Duration;
 
 public class LoginPageTest {
 
@@ -12,6 +21,29 @@ public class LoginPageTest {
     @BeforeAll
     public static void setUp() {
         driver = WebDriverManager.getDriver();
+    }
+
+    @Test
+    public void testStandardUserLogin() {
+        driver.get("https://qa-challenge.codesubmit.io/");
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+
+        Assertions.assertEquals("https://qa-challenge.codesubmit.io/inventory.html", driver.getCurrentUrl());
+    }
+
+    @Test
+    public void testStandardUserFailedLogin() {
+        driver.get("https://qa-challenge.codesubmit.io/");
+        driver.findElement(By.id("user-name")).sendKeys("incorrect_username");
+        driver.findElement(By.id("password")).sendKeys("incorrect_password");
+        driver.findElement(By.id("login-button")).click();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement errorMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("h3[data-test='error']")));
+
+        Assertions.assertEquals("Epic sadface: Username and password do not match any user in this service", errorMessage.getText());
     }
 
     @AfterAll
