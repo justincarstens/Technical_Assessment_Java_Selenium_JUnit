d10310217091f23a9635ebcfcf0473f130f629fd
Justin Carstens <justin.carstens01@gmail.com>
2024-02-10 11:10:24 +0200

    Created WebDriverManager class to allow for easy use throughout the rest of the project

---
+++ OnlineShop/src/test/java/utils/WebDriverManager.java
@@ -0,0 +1,27 @@
+package utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class WebDriverManager {
+
+    private static WebDriver driver;
+
+    public static WebDriver getDriver() {
+
+        ChromeOptions options = new ChromeOptions();
+
+            //Added local Chrome for Testing path to the WebDriver
+        options.setBinary("J:\\Users\\Justin\\Documents\\JAR_FILES\\Selenium\\chrome-win64\\chrome.exe");
+        System.setProperty("webdriver.chrome.driver", "J:\\Users\\Justin\\Documents\\JAR_FILES\\Selenium\\chromedriver-win64\\chromedriver.exe");
+
+        driver = new ChromeDriver(options);
+        return driver;
+
+    }
+
+    public static void closeDriver() {
+        driver.quit();
+    }
+}
